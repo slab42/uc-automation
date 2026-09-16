f6b04f70a9c9b0e2b7cf2b983d3f931273f5c19a
Dan Fox <shiznic@gmail.com>
2026-09-15 17:43:39 -0700

    Updated due to a logging issue.

--- cucm/count_phones_by_device_pool.py
+++ cucm/count_phones_by_device_pool.py
@@ -224,7 +224,16 @@ if __name__ == '__main__':
 
     # Create logs directory if it doesn't exist
     logs_dir = basepath / 'logs'
-    logs_dir.mkdir(exist_ok=True)
+    try:
+        logs_dir.mkdir(exist_ok=True)
+        log_path = logs_dir / (log_filename_prefix + 'temp.log')
+        log_path.touch()  # Test write permissions
+        log_path.unlink()  # Clean up test file
+        log_location = logs_dir
+    except (OSError, PermissionError):
+        # Fall back to script directory if logs directory isn't writable
+        print('Warning: logs directory is not writable, saving to script directory')
+        log_location = basepath
 
     # Get server and login credentials
     cucmInfoFile = input('CUCM JSON File (cucm-info.json): ') or 'cucm-info.json'
@@ -232,8 +241,16 @@ if __name__ == '__main__':
     if password == '':
         password = input(f'Enter CUCM Password for {username}: ')
 
-    # Setup Logging
-    logger = setup_logger(basepath / 'logs' / (log_filename_prefix + cucm + '-' + (time.strftime("%Y_%m_%d-%H_%M_%S")) + '.log'))
+    # Setup Logging - with fallback if permissions fail
+    try:
+        logger = setup_logger(log_location / (log_filename_prefix + cucm + '-' + (time.strftime("%Y_%m_%d-%H_%M_%S")) + '.log'))
+    except PermissionError:
+        # If logging to the preferred location fails, use script directory
+        if log_location != basepath:
+            print('Warning: Could not write to logs directory, saving to script directory instead')
+            logger = setup_logger(basepath / (log_filename_prefix + cucm + '-' + (time.strftime("%Y_%m_%d-%H_%M_%S")) + '.log'))
+        else:
+            raise
 
     # Setup AXL Connection to CUCM
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
